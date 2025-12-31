b3789581df96117773c6ca8bdc1df3ccce1a6991
JenishaEvanjalin <jenishaevanjalinvedhanayagam@gmail.com>
2025-12-31 12:52:00 +0530

    to do list ui creation

---
+++ src/components/todo list/TodoCard.jsx
@@ -0,0 +1,10 @@
+export default function TodoCard(){
+    return(
+        <div>
+        <h2>To-Do List</h2>
+        <input type="text" placeholder="Enter your text"/>
+        <button>Add</button>
+        <div>No task yet</div>
+        </div>
+    )
+}
